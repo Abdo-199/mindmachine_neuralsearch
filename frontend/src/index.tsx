d8307236680e03899c961517846151856c0e5576
Pascal Wende <s0577743@htw-berlin.de>
2023-10-29 08:33:07 +0100

    Init with Typescript

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
     <div>
-        <p>Hallo! Hier kommt die erste Komponente rein!</p>
+      Hier erstes Element bitte!
     </div>
 );
 
