59243819b67ddfbdf1f15a3f04ee557bb9a76de7
Pascal Wende <s0577743@htw-berlin.de>
2023-11-05 10:17:43 +0100

    File-Information Page erstellt

--- frontend/src/components/Misc/MainWindow.tsx
+++ frontend/src/components/Misc/MainWindow.tsx
@@ -3,19 +3,19 @@ import Header from './Header'
 import Footer from './Footer'
 import HomeWindow from '../Home/HomeWindow'
 import '../../styles/Misc/MainWindow.css'
+import FileInformationWindow from '../FileInformation/FileInformationWindow'
+import LegalNotice from './LegalNotice'
 
 const MainWindow = () => {
   return (
-    <div id="test">
-
-
-    <div id="mainWindow-container">
+    <div id="windowWrapper">
+      <div id="mainWindow-container">
         <Header></Header>
         <div id="mainContent">
-            <HomeWindow></HomeWindow>
+          <HomeWindow></HomeWindow>
         </div>
         <Footer></Footer>
-    </div>
+      </div>
     </div>
   )
 }
