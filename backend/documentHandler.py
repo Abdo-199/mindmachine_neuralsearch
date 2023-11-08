3927c2cf6011b78e37372d842e3e6ba00b6876b0
Moritz Köhler <moritz.koehler@student.htw-berlin.de>
2023-11-08 13:45:22 +0000

    Backend Setup

---
+++ backend/documentHandler.py
@@ -0,0 +1,22 @@
+
+
+class DocumentHandler:
+
+    def __init__(self, user_id, document_id) -> None:
+        self.user_id = user_id
+        self.document_id = document_id
+
+    def pdf_to_text(self):
+        pass
+
+    def docx_to_text(self):
+        pass
+
+    def encode_text(self):
+        pass
+
+    def decode_text(self):
+        pass
+
+    def get_document_from_file(self):
+        pass
