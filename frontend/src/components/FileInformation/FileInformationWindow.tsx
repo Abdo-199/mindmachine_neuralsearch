1d42670fc7d1e343071cfe623f800b9cae232ff0
Julia Michajlow <julia.michajlow@student.htw-berlin.de>
2023-11-15 16:18:07 +0000

    Dateinamen ändern

--- frontend/src/components/FileInformation/FileInformationWindow.tsx
+++ frontend/src/components/FileInformation/FileInformationWindow.tsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import '../../styles/FileInformation/FileInformation.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFile } from '@fortawesome/free-solid-svg-icons'
-import FileOptions from './FileOptions'
-import FileInfosCard from './FileInfosCard'
+import "../../styles/FileInformation/FileInformation.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import FileOptions from "./FileOptions";
+import FileInfosCard from "./FileInfosCard";
+import { useState } from "react";
 
 const FileInformationWindow = () => {
-    return (
-        <div id="file-information-wrapper">
-            <div id="iconCard">
-                <FontAwesomeIcon icon={faFile} style={{ fontSize: "9rem" }} />
-            </div>
-            <div id="fileInfosCard-wrapper">
-                <FileInfosCard></FileInfosCard>
-                <FileOptions></FileOptions>
-            </div>
-        </div>
-    )
-}
+  const [filename, SetFilename] = useState("MeinDokument");
+  const [modalHandlerDataChange, setModalHandlerDataChange] = useState(false);
 
-export default FileInformationWindow
+  const ModalHandlerDataChange = () => {
+    // Modalhanlder zum Öffnen/Schließen der Diagrammeinstellungen
+    setModalHandlerDataChange((current) => !current);
+  };
+
+  return (
+    <>
+      <div id="file-information-wrapper">
+        <div id="iconCard">
+          <FontAwesomeIcon icon={faFile} style={{ fontSize: "9rem" }} />
+        </div>
+        <div id="fileInfosCard-wrapper">
+          <FileInfosCard filename={filename}></FileInfosCard>
+          <FileOptions
+            modalHandlerDataChange={modalHandlerDataChange}
+            ModalHandlerDataChange={ModalHandlerDataChange}
+            filename={filename}
+            SetFilename={SetFilename}
+          ></FileOptions>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FileInformationWindow;
--- frontend/src/components/FileInformation/FileInfosCard.tsx
+++ frontend/src/components/FileInformation/FileInfosCard.tsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react'
-import '../../styles/FileInformation/FileInformation.css'
+import React, { useState } from "react";
+import "../../styles/FileInformation/FileInformation.css";
 
-const FileInfosCard = () => {
-
-    const [filename, setFileName] = useState("MeinDokument");
-    const [size, setSize] = useState("5.3MB");
-    const [dateAdded, setDateAdded] = useState("05.11.2023");
-    const [description, setDescription] = useState("Blablabla");
+const FileInfosCard = ({filename}: {filename: string}) => {
+    
+  const [size, setSize] = useState("5.3MB");
+  const [dateAdded, setDateAdded] = useState("05.11.2023");
+  const [description, setDescription] = useState("Blablabla");
 
   return (
-    <div id="fileInfosCard">
-        <p><span>Filename: </span>{filename}</p>
-        <p><span>Size: </span>{size}</p>
-        <p><span>Date added: </span>{dateAdded}</p>
-        <p><span>Description: </span>{description}</p>
-    </div>
-  )
-}
+    <>
+      <div id="fileInfosCard">
+        <p>
+          <span>Filename: </span>
+          {filename}
+        </p>
+        <p>
+          <span>Size: </span>
+          {size}
+        </p>
+        <p>
+          <span>Date added: </span>
+          {dateAdded}
+        </p>
+        <p>
+          <span>Description: </span>
+          {description}
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default FileInfosCard
+export default FileInfosCard;
