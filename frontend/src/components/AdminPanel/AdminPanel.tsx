4390e5914c9919bec320722d00512b7370ef13e2
Marouane Lamloumi <marouane.lamloumi@student.htw-berlin.de>
2023-11-20 08:28:33 +0000

    Resolve "Admin Page erstellen"

--- frontend/src/components/AdminPanel/AdminPanel.tsx
+++ frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -1,9 +1,153 @@
-import React from 'react'
-const AdminPanel = () => {
-    return (
-    <div >
-    
-    </div>
-    )
+import { Card } from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+
+import "../../styles/SearchResult/SearchResult.css";
+import React from "react"
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import BasicPie from "./PieChart";
+import Button from "@mui/material/Button";
+
+import { Grid, Typography, TextField, Box } from "@mui/material";
+import { width } from "@mui/system";
+
+function createData(
+  Users: string,
+  Email: string,
+  Storage_usage: number,
+  Last_access: Date,
+  Status: string
+) {
+  return { Users, Email, Storage_usage, Last_access, Status };
 }
-export default AdminPanel
+const rows = [
+  createData("User01", "s0550199@htw-berlin.de", 65, new Date("10-23-2011"), "User"),
+  createData("User02", "s0550299@htw-berlin.de", 93, new Date("11-12-2023"), "Admin"),
+  createData("User03", "s0550399@htw-berlin.de", 43, new Date(), "User"),
+  
+];
+
+const AdminPanel = () => {
+  
+  return (
+    <div>
+      <h4 style={{ textAlign:"center" }}>Statistics</h4>
+      <h4 style={{ marginLeft: "100px" }}>Storage</h4>
+      <Card style={{ marginLeft:100, marginRight:100 }} variant="elevation">
+        <CardContent>
+          <Grid container>
+            {/* First Column */}
+            <Grid item xs={12} sm={5}>
+              <CardContent>
+                <BasicPie></BasicPie>
+              </CardContent>
+            </Grid>
+
+            {/* Second Column */}
+            <Grid item xs={12} sm={7}>
+              <Card
+                style={{
+                  marginLeft: "150px",
+                  marginRight: "300px",
+                  minWidth: "500px",
+                }}
+              >
+                <h4 style={{textAlign:"center"}}>Storage manager</h4>
+                <Grid container style={{maxWidth:"300px"}}>
+                  {/* First Row */}
+                  <Grid item xs={12} style={{marginLeft:"50px"}}></Grid>
+                  Limit storage usage at{" "}
+                  <Box component="span" display="inline-block">
+                    <TextField
+                      sx={{ width: "50px" }}
+                      variant="outlined"
+                      size="small"
+                        
+                    />
+                  </Box>
+                </Grid>
+                <Grid container>
+                  {/* First Row */}
+                  <Grid item xs={12}></Grid>
+                  
+                  Limit storage usage for every user{" "}
+                  <Box component="span" display="inline-block">
+                    <TextField
+                      sx={{ width: "50px" }}
+                      variant="outlined"
+                      size="small"
+                    />
+                  </Box>
+                  
+                </Grid>
+                <Grid container>
+                  {/* First Row */}
+                  <Grid item xs={12}></Grid>
+              
+                  <Box >
+                  <Button style={{ color : "black" , backgroundColor: "#83b600" , marginLeft:"120px", marginBottom:"15px"}}>Open PDF</Button>
+                  </Box>
+                </Grid>
+              </Card>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+
+      
+
+      <h4 style={{ marginLeft: "100px", marginTop:"30px"}}>Audio Logout</h4>
+      <Card
+        style={{ marginLeft: "100px", marginRight: "300px", minWidth: "500px" , marginTop:"20px" }}
+      >
+        set auto log out after{" "}
+        <Box component="span" display="inline-block">
+          <TextField sx={{ width: "50px" }} variant="outlined" size="small" />
+        </Box>{" "}
+        minutes
+      </Card>
+      <h4 style={{ marginLeft:"100px", marginTop:"30px" }}>Statistics</h4>
+
+      
+      
+      
+      <Card style={{minWidth: "650px" , marginLeft:"100px", marginRight:"100px", marginTop:"20px"}}>
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ minWidth: "650px" , marginLeft:"20px", marginRight:"20px" }}
+          aria-label="simple table"
+        >
+          <TableHead>
+            <TableRow >
+              <TableCell sx={{ width: '80px' }} align="left" >Users</TableCell>
+              <TableCell sx={{ width: '100px' }} align="left">Email</TableCell>
+              <TableCell sx={{ width: '30px' }} align="left">User storage</TableCell>
+              <TableCell sx={{ width: '30px' }} align="left">Last access</TableCell>
+              <TableCell sx={{ width: '30px' }} align="left">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow>            
+                <TableCell align="left">{row.Users}</TableCell>
+                <TableCell align="left">{row.Email}</TableCell>
+                <TableCell align="left">{row.Storage_usage}</TableCell>
+                <TableCell align="left">{row.Last_access.toLocaleDateString()}</TableCell>
+                <TableCell align="left">{row.Status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+
+        </Table>
+      </TableContainer>
+      </Card>
+    </div>
+  );
+};
+export default AdminPanel;
--- frontend/src/components/Misc/MainWindow.tsx
+++ frontend/src/components/Misc/MainWindow.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import HomeWindow from "../Home/HomeWindow";
+import React, { useEffect, useState } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import HomeWindow from '../Home/HomeWindow'
 
-import "../../styles/Misc/MainWindow.css";
-import LegalNotice from "./LegalNotice";
-import SearchHistory from "../SearchHistory/SearchHistory";
-import SearchResult from "../SearchResult/SearchResult";
-import AdminPanel from "../AdminPanel/AdminPanel";
-import FileInformationWindow from "../FileInformation/FileInformationWindow";
+import '../../styles/Misc/MainWindow.css'
+import LegalNotice from './LegalNotice';
+import SearchHistory from '../SearchHistory/SearchHistory';
+import SearchResult from '../SearchResult/SearchResult';
+import AdminPanel from '../AdminPanel/AdminPanel';
+import FileInformationWindow from '../FileInformation/FileInformationWindow'
 import { useNavigate } from "react-router-dom";
 
 interface MainWindowProps {
   content: string;
 }
 
-
 const MainWindow: React.FC<MainWindowProps> = ({ content }) => {
+
   const navigate = useNavigate();
 
   // Liste aller Dateien (noch nicht user-spezifisch)
@@ -24,45 +24,48 @@ const MainWindow: React.FC<MainWindowProps> = ({ content }) => {
 
   useEffect(() => {
     if (localStorage.getItem("userID") == null || localStorage.getItem("isAdmin") == null) {
-      navigate("/");
+      navigate("/")
     }
     else {
       GetFileStructure();
     }
-  }, []);
+  }, [])
 
-  const GetFileStructure = async () => {
-    return await fetch(`http://localhost:8000/filestructure/${localStorage.getItem("userID")}`, {
-      method: 'GET',
-      mode: "cors",
-      cache: "no-cache",
+const GetFileStructure = async () => {
+  return await fetch(`http://localhost:8000/filestructure/${localStorage.getItem("userID")}`, {
+    method: 'GET',
+    mode: "cors",
+    cache: "no-cache",
+  })
+    .then(res => res.json())
+    .then(response => {
+      SetDocRows(response)
     })
-      .then(res => res.json())
-      .then(response => {
-        SetDocRows(response)
-      })
-  }
+}
 
-  const componentMap: { [key: string]: React.ReactElement } = {
-    "HomeWindow": <HomeWindow docRows={docRows} SetDocRows={SetDocRows} />,
-    "LegalNotice": <LegalNotice />,
-    "SearchHistory": <SearchHistory />,
-    "SearchResult": <SearchResult />,
-    "AdminPanel": <AdminPanel />,
-    "FileInformation": <FileInformationWindow docRows={docRows} SetDocRows={SetDocRows} />,
-  };
-
-  const toRenderContent = componentMap[content] || <div>Component not found</div>;
-
-  return (
-    <div id="windowWrapper">
-      <div id="mainWindow-container">
-        <Header></Header>
-        <div id="mainContent">{toRenderContent}</div>
-        <Footer></Footer>
-      </div>
-    </div>
-  );
+//Prüfe, was angezeigt werden soll
+const componentMap: { [key: string]: React.ReactElement } = {
+  "HomeWindow": <HomeWindow docRows={docRows} SetDocRows={SetDocRows} />,
+  "LegalNotice": <LegalNotice />,
+  "SearchHistory": <SearchHistory />,
+  "SearchResult": <SearchResult />,
+  "AdminPanel": <AdminPanel />,
+  "FileInformation": <FileInformationWindow docRows={docRows} SetDocRows={SetDocRows} />,
 };
 
-export default MainWindow;
+const toRenderContent = componentMap[content] || <div>Component not found</div>;
+
+return (
+  <div id="windowWrapper">
+    <div id="mainWindow-container">
+      <Header></Header>
+      <div id="mainContent">
+        {toRenderContent}
+      </div>
+      <Footer></Footer>
+    </div>
+  </div>
+)
+    }
+
+export default MainWindow
