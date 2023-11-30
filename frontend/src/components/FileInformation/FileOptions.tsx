1cd09a529d1857e6fa3ff6552e6cf5d9142efb65
Julia Michajlow <m.julie99@web.de>
2023-11-30 14:21:12 +0100

    display pdf

--- frontend/src/components/FileInformation/FileOptions.tsx
+++ frontend/src/components/FileInformation/FileOptions.tsx
@@ -25,73 +25,81 @@ const FileOptions = ({
   docRows: any[];
   SetDocRows: any;
 }) => {
-  
   const [newFilename, SetNewFilename] = useState("");
   const [isFileOpened, setIsFileOpened] = useState<boolean>(false);
-  const [numPages, setNumPages] = useState<number[]>([1]);
-  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const [pdfURL, setPDFURL] = useState<string>("");
 
   const [modalHandlerDataChange, setModalHandlerDataChange] = useState(false);
   const [modalHandlerDataDelete, setModalHandlerDataDelete] = useState(false);
-  
+
   const [isConfirmed, SetIsConfirmed] = useState(false);
-  
-  const navigate = useNavigate();
-  
-  // Dokument erfolgreich geöffnet
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    const numpages = Array.from({ length: numPages + 1 }, (_, index) => (index += 1)
-    );
-    setNumPages(numpages);
-  }
-  // Modalhandler zum Ändern des Dateinamens
+
+  // open and close dialog for renaming a file
   const ModalHandlerDataChange = () => {
     setModalHandlerDataChange((current) => !current);
   };
 
-  // Modalhandler zum Löschen der Datei
+  // open and close dialog for deleting a file
   const ModalHandlerDataDelete = () => {
     setModalHandlerDataDelete((current) => !current);
   };
 
-  // Modalhandler zum Öffnen der PDF
+  // open and close pdf-viewer for displaying a pdf
   const IsFileOpened = () => {
     setIsFileOpened((current) => !current);
   };
 
-
+  // get pdf file and display it
   const OpenFile = () => {
     IsFileOpened();
+    API_GetDocument();
   };
 
+  // rename a file
   const RenameFile = () => {
     if (newFilename != "") {
       SetThisFile((prevFileInfo: any) => ({
         ...prevFileInfo,
         file_name: newFilename,
       }));
-
       API_EditDocumentName();
-      // Bestätigung über Umbenennung an Nutzer senden
       SetIsConfirmed(true);
     }
   };
 
+  // delete a file
   const DeleteFile = () => {
     const fileFound = docRows.find((file) => file.file_name == filename);
 
     if (fileFound) {
       SetThisFile(null);
       API_DeleteDocument();
-      // Bestätigung über Löschung an Nutzer senden
       SetIsConfirmed(true);
       ModalHandlerDeleteConfirm();
-      console.log(modalHandlerDeleteConfirm);
     } else {
       alert("Error. There was a problem.");
     }
   };
 
+  // retrieve document from backend
+  const API_GetDocument = async () => {
+    const user_id = localStorage.getItem("userID");
+
+    const fetchString = `http://141.45.224.114:8000/document?user_id=${user_id}&document_name=${filename}`;
+    return await fetch(fetchString, {
+      method: "GET",
+    }).then(async (res) => {
+      const blob = await res.blob();
+      const pdfUrl = URL.createObjectURL(blob);
+      console.log(blob);
+      console.log(pdfUrl);
+      console.log(filename);      
+      setPDFURL(pdfUrl);
+    });
+  };
+
+  // send delete request to backend
   const API_DeleteDocument = async () => {
     return await fetch(
       `http://141.45.224.114:8000/deleteDocument/${localStorage.getItem(
@@ -110,6 +118,7 @@ const FileOptions = ({
       });
   };
 
+  // send rename request to backend
   const API_EditDocumentName = async () => {
     return await fetch(
       `http://141.45.224.114:8000/editDocumentName/${localStorage.getItem(
@@ -141,32 +150,17 @@ const FileOptions = ({
 
   return (
     <>
-      {isFileOpened && (
-        <>
-          <Modal
-            header={""}
-            content={
-              <div>
-                <br></br>
-                <Document
-                  file={"/sample.pdf"}
-                  onLoadSuccess={onDocumentLoadSuccess}
-                >
-                  {numPages.map((page) => (
-                    <Page
-                      renderMode="svg"
-                      pageNumber={page}
-                      renderTextLayer={true}
-                      renderAnnotationLayer={false}
-                    />
-                  ))}
-                </Document>
-              </div>
-            }
-            closeModal={IsFileOpened}
-          ></Modal>
-        </>
-      )}
+      <div id="fileOptions-buttons">
+        <button className="fileOption-button" onClick={() => OpenFile()}>
+          Open file
+        </button>
+        <button className="fileOption-button" onClick={ModalHandlerDataChange}>
+          Rename
+        </button>
+        <button className="fileOption-button" onClick={ModalHandlerDataDelete}>
+          Delete
+        </button>
+      </div>
       {modalHandlerDataChange ? (
         <RenameFileModal
           RenameFile={RenameFile}
@@ -186,17 +180,14 @@ const FileOptions = ({
           closeModal={ModalHandlerDataDelete}
         ></DeleteFileModal>
       ) : null}
-      <div id="fileOptions-buttons">
-        <button className="fileOption-button" onClick={() => OpenFile()}>
-          Open file
-        </button>
-        <button className="fileOption-button" onClick={ModalHandlerDataChange}>
-          Rename
-        </button>
-        <button className="fileOption-button" onClick={ModalHandlerDataDelete}>
-          Delete
-        </button>
-      </div>
+      {isFileOpened && (
+        <iframe
+          title="pdf-viewer"
+          width={800}
+          height={1000}
+          src={pdfURL}
+        ></iframe>
+      )}
     </>
   );
 };
