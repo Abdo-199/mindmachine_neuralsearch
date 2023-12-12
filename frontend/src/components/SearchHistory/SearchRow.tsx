9636129dc11970dff0e10080540657a06b6e1b87
s0579418 <s0579418@htw-berlin.de>
2023-12-12 17:59:55 +0100

    refractored code to SearchRow

---
+++ frontend/src/components/SearchHistory/SearchRow.tsx
@@ -0,0 +1,26 @@
+import { MouseEventHandler, useState } from "react";
+import "../../styles/SearchHistory/SearchHistory.css";
+
+interface SearchRowProps {
+  name: string;
+  createdOn: string;
+  Search: any;
+  Delete: any;
+}
+
+const SearchRow = ({ name, createdOn, Search, Delete }: SearchRowProps) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{createdOn}</td>
+      <td>
+        <button onClick={() => Search(name)}>Search</button>
+      </td>
+      <td>
+        <button onClick={() => Delete(name)}>Delete</button>
+      </td>
+    </tr>
+  );
+};
+
+export default SearchRow;
